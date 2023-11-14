f5700e1754d6563d373ca1c1417553cfba371c74
cr2115 <cr2115@engr.ship.edu>
2023-11-14 15:11:22 -0500

    connected basic gui to client. still need shared secret calc fix, broadcast messages to all client chatboxes

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
 import socket
+import threading
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import ElGamal
 from Crypto import Random
 from Crypto.Util.Padding import pad, unpad
+import tkinter as tk
+from tkinter import scrolledtext
 
-def start_client():
+def start_client(gui):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 8082))
     print("Connected to server.")
@@ -13,15 +16,14 @@ def start_client():
     # Generate ElGamal keys for Diffie-Hellman
     key = ElGamal.generate(256, Random.new().read)
 
+    # Send client's public key to server
+    client_socket.send(f"{key.p},{key.g},{key.y}".encode())
+
     # Receive server's public key
     received_data = client_socket.recv(1024).decode()
     print(f"Client received: {received_data}")
-    print(f"Client received data length: {len(received_data)}")
     p, g, y = map(int, received_data.split(","))
     server_public_key = ElGamal.construct((p, g, y))
-
-    # Send client's public key to server
-    client_socket.send(f"{key.p},{key.g},{key.y}".encode())
 
     # Compute shared secret
     shared_secret = pow(server_public_key.y, int(key.x), int(key.p))
@@ -32,39 +34,92 @@ def start_client():
 
     # Receive and decrypt welcome message
     encrypted_response = client_socket.recv(1024)
-    print(f"Client received encrypted data length: {len(encrypted_response)}")
-    print(f"Client received: {encrypted_response.hex()}")
     iv = encrypted_response[:16]
     cipher = AES.new(aes_key, AES.MODE_CBC, iv)
     decrypted_data = cipher.decrypt(encrypted_response[16:])
-    print("Decrypted data length:", len(decrypted_data))
-    print("Decrypted data (hex):", decrypted_data.hex())
     decrypted_message = unpad(decrypted_data, AES.block_size).decode()
     print(f"Server says: {decrypted_message}")
-
-    try:
+    gui.update_chat(f"Server says: {decrypted_message}")
+    def receive_messages():
         while True:
-            message = input("Enter your message: ")
-            iv = Random.new().read(16)  # Generate new IV for every message
-            cipher = AES.new(aes_key, AES.MODE_CBC, iv)
-            ct_bytes = cipher.encrypt(pad(message.encode(), AES.block_size))
-            encrypted_message = iv + ct_bytes
-            client_socket.send(encrypted_message)
+            try:
+                encrypted_response = client_socket.recv(1024)
+                if encrypted_response:
+                    iv_response = encrypted_response[:16]
+                    cipher_response = AES.new(aes_key, AES.MODE_CBC, iv=iv_response)
+                    decrypted_response = cipher_response.decrypt(encrypted_response[16:])
+                    response = unpad(decrypted_response, AES.block_size).decode()
+                    gui.update_chat(response)
+            except OSError:  # Possibly client has left the chat.
+                break
 
-            encrypted_response = client_socket.recv(1024)
-            print(f"Client received encrypted data length: {len(encrypted_response)}")
-            iv_response = encrypted_response[:16]
-            cipher_response = AES.new(aes_key, AES.MODE_CBC, iv=iv_response)
-            decrypted_response = cipher_response.decrypt(encrypted_response[16:])
-            print("Decrypted data length:", len(decrypted_response))
-            print("Decrypted data (hex):", decrypted_response.hex())
-            response = unpad(decrypted_response, AES.block_size).decode()
-            print(f"Server responded: {response}")
+    threading.Thread(target=receive_messages).start()
 
-    except KeyboardInterrupt:
-        print("\nDisconnecting from the server...")
-    finally:
+    def send_message(message):
+        iv = Random.new().read(16)
+        cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+        ct_bytes = cipher.encrypt(pad(message.encode(), AES.block_size))
+        encrypted_message = iv + ct_bytes
+        client_socket.send(encrypted_message)
+
+    def on_closing():
         client_socket.close()
+        gui.root.destroy()
+
+    gui.set_send_function(send_message)
+    gui.root.protocol("WM_DELETE_WINDOW", on_closing)
+
+class ChatGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("P2P Chat Client")
+
+        self.chat_log = scrolledtext.ScrolledText(self.root, state='disabled')
+        self.chat_log.grid(row=0, column=0, columnspan=2)
+
+        self.msg_entry = tk.Entry(self.root)
+        self.msg_entry.grid(row=1, column=0)
+
+        self.send_button = tk.Button(self.root, text="Send", command=self.send_msg)
+        self.send_button.grid(row=1, column=1)
+
+        self.send_function = None
+
+        # Start a thread for listening to messages from the server
+        threading.Thread(target=self.receive_messages, daemon=True).start()
+
+    def receive_messages(self):
+        while True:
+            try:
+                # Assuming 'client_socket' is your socket object and 'aes_key' is your AES key
+                encrypted_response = client_socket.recv(1024)
+                if encrypted_response:
+                    iv = encrypted_response[:16]
+                    cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+                    decrypted_response = unpad(cipher.decrypt(encrypted_response[16:]), AES.block_size).decode()
+
+                    # Use Tkinter's thread-safe method to update the chat window
+                    self.chat_log.insert(tk.END, decrypted_response + '\n')
+            except Exception as e:
+                print(f"Error receiving message: {e}")
+                break
+
+    def set_send_function(self, send_function):
+        self.send_function = send_function
+
+    def send_msg(self):
+        message = self.msg_entry.get()
+        if message and self.send_function:
+            self.send_function(message)
+            self.msg_entry.delete(0, tk.END)
+
+    def update_chat(self, message):
+        self.chat_log.config(state='normal')
+        self.chat_log.insert(tk.END, message + '\n')
+        self.chat_log.config(state='disabled')
+        self.chat_log.yview(tk.END)
 
 if __name__ == "__main__":
-    start_client()
+    gui = ChatGUI()
+    start_client(gui)
+    gui.root.mainloop()
--- server.py
+++ server.py
@@ -6,61 +6,71 @@ from Crypto.PublicKey import ElGamal
 from Crypto import Random
 from Crypto.Util.Padding import pad, unpad
 
-def handle_client(client_socket, addr):
-    print(f"Connection established with {addr}")
+class ClientHandler:
+    def __init__(self):
+        self.clients = {}
+        self.client_count = 0
 
-    # Generate ElGamal keys for Diffie-Hellman
-    key = ElGamal.generate(256, Random.new().read)
-    public_key = key.publickey()
+    def add_client(self, client_socket, addr):
+        self.client_count += 1
+        client_id = f"Client_{self.client_count}"  # Unique identifier for each client
+        self.clients[client_id] = (client_socket, addr)
+        return client_id
 
-    # Send server's public key to client
-    client_socket.send(f"{public_key.p},{public_key.g},{public_key.y}".encode())
+    def remove_client(self, client_id):
+        if client_id in self.clients:
+            del self.clients[client_id]
 
-    # Receive client's public key
-    received_data = client_socket.recv(1024).decode()
-    print(f"Server received: {received_data}")
-    print(f"Server received data length: {len(received_data)}")
-    p, g, y = map(int, received_data.split(","))
-    client_public_key = ElGamal.construct((p, g, y))
+    def handle_client(self, client_socket, addr):
+        print(f"Connection established with {addr}")
 
-    # Compute shared secret
-    shared_secret = pow(client_public_key.y, int(key.x), int(key.p))
-    print(f"Server Shared Secret: {shared_secret}")
-    shared_secret = 123456789
-    aes_key = SHA256.new(str(shared_secret).encode()).digest()
-    print(f"Server AES Key: {aes_key.hex()}")
+        # Generate ElGamal keys for Diffie-Hellman
+        key = ElGamal.generate(256, Random.new().read)
+        public_key = key.publickey()
 
-    # Encrypt a welcome message
-    iv = Random.new().read(16)
-    cipher = AES.new(aes_key, AES.MODE_CBC, iv)
-    padded_message = pad("Welcome to the secure chat!".encode(), 16)
-    encrypted_data = iv + cipher.encrypt(padded_message)
-    print("Padded message length:", len(padded_message))
-    print("Padded message (hex):", padded_message.hex())
-    print(f"Server encrypted data length: {len(encrypted_data)}")
-    print(f"Sending from server: {encrypted_data.hex()}")
-    client_socket.send(encrypted_data)
+        # Send server's public key to client
+        client_socket.send(f"{public_key.p},{public_key.g},{public_key.y}".encode())
 
-    try:
-        while True:
-            iv = Random.new().read(16)  # Generate new IV for every message
-            encrypted_data = client_socket.recv(1024)
-            iv_received = encrypted_data[:16]
-            cipher = AES.new(aes_key, AES.MODE_CBC, iv=iv_received)
-            decrypted_data = unpad(cipher.decrypt(encrypted_data[16:]), AES.block_size).decode()
+        # Receive client's public key
+        received_data = client_socket.recv(1024).decode()
+        p, g, y = map(int, received_data.split(","))
+        client_public_key = ElGamal.construct((p, g, y))
 
-            print(f"Received from {addr}: {decrypted_data}")
-            response = "Message received!"
-            cipher = AES.new(aes_key, AES.MODE_CBC, iv)
-            ct_bytes = cipher.encrypt(pad(response.encode(), AES.block_size))
-            encrypted_response = iv + ct_bytes
-            client_socket.send(encrypted_response)
+        # Compute shared secret
+        shared_secret = pow(client_public_key.y, int(key.x), int(key.p))
+        shared_secret = 123456789
+        aes_key = SHA256.new(str(shared_secret).encode()).digest()
 
-    except Exception as e:
-        print(f"Error with connection from {addr}: {e}")
-    finally:
-        print(f"Connection from {addr} closed.")
-        client_socket.close()
+        # Encrypt a welcome message
+        iv = Random.new().read(16)
+        cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+        padded_message = pad("Welcome to the secure chat!".encode(), 16)
+        encrypted_data = iv + cipher.encrypt(padded_message)
+        client_socket.send(encrypted_data)
+
+        client_id = self.add_client(client_socket, addr)
+
+        try:
+            while True:
+                iv = Random.new().read(16)  # Generate new IV for every message
+                encrypted_data = client_socket.recv(1024)
+                iv_received = encrypted_data[:16]
+                cipher = AES.new(aes_key, AES.MODE_CBC, iv=iv_received)
+                decrypted_data = unpad(cipher.decrypt(encrypted_data[16:]), AES.block_size).decode()
+
+                print(f"Received from {addr}: {decrypted_data}")
+                response = f"{client_id}: {decrypted_data}"
+                cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+                ct_bytes = cipher.encrypt(pad(response.encode(), AES.block_size))
+                encrypted_response = iv + ct_bytes
+                client_socket.send(encrypted_response)
+
+        except Exception as e:
+            print(f"Error with connection from {addr}: {e}")
+        finally:
+            print(f"Connection from {addr} closed.")
+            self.remove_client(client_id)
+            client_socket.close()
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -69,13 +79,13 @@ def start_server():
     server_socket.listen(5)
     print("Server started and waiting for connections...")
 
+    handler = ClientHandler()
+
     try:
         while True:
             client_socket, addr = server_socket.accept()
-            # Start a new thread for each client connection
-            client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+            client_thread = threading.Thread(target=handler.handle_client, args=(client_socket, addr))
             client_thread.start()
-            print(f"Active connections: {threading.active_count() - 1}")
     except KeyboardInterrupt:
         print("\nShutting down the server...")
     finally:
